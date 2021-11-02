e9aa88a2a17127ab4747ec19340903fdfa925e93
esther615kim <vilchen@naver.com>
2021-11-02 09:06:37 +0900

    Feat: create productApi.js

--- src/redux/features/productSlice.js
+++ src/redux/features/productSlice.js
@@ -1,10 +1,19 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import axios from 'axios'
 
 const initialState = {
     items: [],
-    status: null // 왜 null??
-
+    status: null, // 왜 null??
+    error: null // undefined 아니고 null 주는 이유는 
 }
+// asyncthunk 함수
+export const productsFetch = createAsyncThunk(
+    "products/productsFetch",
+    async () => {
+        const response = await axios.get("https://fakestoreapi.com/products");
+        return response?.data;
+    }
+)
 
 const productReducer = createSlice({
     name: 'product',
@@ -13,4 +22,4 @@ const productReducer = createSlice({
     },
 })
 
-export default productReducer.reducer
+export default productReducer.reducer // 여기 이렇게 말고 다른 export 방법은?
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit'
-import productReducer from './features/productSlice';
+import productReducer, { productsFetch } from './features/productSlice';
+// import { setupListeners } from '@reduxjs/toolkit/query'
+import { productApi } from './features/productApi';
 
 export const store = configureStore({
     reducer: {
         products: productReducer,
+        [productApi.reducerPath]: productApi.reducer,
     },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(productApi.middleware), // 여기 뭔지 모름
 })
+
+store.dispatch(productsFetch());// 왜 여기서 해줌?
+
+// setupListeners(store.dispatch) //아님 이거 씀 독스..
