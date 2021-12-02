e62f68ca45cad84bd94e9956556e0628507a52c8
esther615kim <vilchen@naver.com>
2021-12-02 12:28:51 +0900

    Delete : remove unused data variables from Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import { Slides, ProductList } from '../components/index';
 import { useGetProductsQuery } from '../redux/features/productsApi';
 const Home = () => {
-  const { data, error, isLoading } = useGetProductsQuery;
+  //   const { data, error, isLoading } = useGetProductsQuery;
+  const { data } = useGetProductsQuery;
+  console.log(data);
   return (
     <div>
       <Slides />
       <ProductList />
+      <div>{data}</div>
     </div>
   );
 };
