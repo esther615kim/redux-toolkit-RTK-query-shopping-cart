4fc25e48bfc0b031359f4f4aba72b643c15fb2a2
esther615kim <vilchen@naver.com>
2021-12-02 11:55:37 +0900

    Refactor: productsSlice.js with createAsyncThunk

--- src/redux/features/productsSlice.js
+++ src/redux/features/productsSlice.js
@@ -3,50 +3,53 @@ import axios from 'axios'
 
 const initialState = {
     items: [],
-    status: null, // 왜 null??
-    // error: null // undefined 아니고 null 주는 이유는 
+    status: null,
+    error: null
 }
+
 // asyncthunk 함수
 export const productsFetch = createAsyncThunk(
+
     "products/productsFetch",
-    async () => {
-        const response = await axios.get("https://fakestoreapi.com/products");
-        return response?.data;
+    async ({ rejectWithValue }) => {
+        try {
+            const response = await axios.get(
+                "https://fakestoreapi.com/productss"
+            );
+            return response?.data;
+        } catch (error) {
+            return rejectWithValue("An error has occured");
+            // console.log(error);
+        }
     }
 )
-//     "products/productsFetch",
-//     async () => {
-//         try {
-//             const response = await axios.get(
-//                 "https://fakestoreapi.com/products"
-//             );
-//             return response.data;
-//         } catch (error) {
-//             console.log(error);
-//         }
-//     }
-// );
 
+// before adding params in productsFetch
+// "products/productsFetch",
+// async () => {
+//     const response = await axios.get("https://fakestoreapi.com/products");
+//     return response?.data;
+// }
 
 const productsSlice = createSlice({
     name: 'products', // line 11에 들어감
     initialState,
-    reducers: {
+    reducers: {},
+    extraReducers: {
+        [productsFetch.pending]: (state) => {
+            state.status = "pending";
+        },
+        [productsFetch.fulfilled]: (state, action) => {
+            state.items = action.payload;
+            state.status = "success";
+        },
+        [productsFetch.rejected]: (state, action) => {
+            state.status = "rejected";
+            state.error = action.payload;
+        },
     },
-    // extraReducers: {
-    //     [productsFetch.pending]: (state) => {
-    //         state.status = "pending";
-    //     },
-    //     [productsFetch.fulfilled]: (state, action) => {
-    //         state.items = action.payload;
-    //         state.status = "success";
-    //     },
-    //     [productsFetch.rejected]: (state) => {
-    //         state.status = "rejected";
-    //     },
-    // },
 })
 
-export default productsSlice.reducer // 여기 이렇게 말고 다른 export 방법은?
+export default productsSlice.reducer
 
 
--- src/redux/store.js
+++ src/redux/store.js
@@ -3,16 +3,15 @@ import { configureStore } from '@reduxjs/toolkit';
 import productsSlice, { productsFetch } from './features/productsSlice';
 import { productsApi } from './features/productsApi';
 // import { setupListeners } from '@reduxjs/toolkit/query'
+
 export const store = configureStore({
     reducer: {
         products: productsSlice,
-        [productsApi.reducerPath]: productsApi.reducer,
+        // [productsApi.reducerPath]: productsApi.reducer,
     },
     // configure custom middleware
     middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware().concat(productsApi.middleware), // 여기 ...읭?
+        getDefaultMiddleware().concat(productsApi.middleware),
 })
 
-store.dispatch(productsFetch());// ?? 아래 꺼랑 차이는
-
-// setupListeners(store.dispatch) //아님 이거 씀 독스..
+store.dispatch(productsFetch());
